15f176fb48f4f6ea1f6b4586774c8abbec81af9d
Soumyadip <bhowmik.soumyadip9@gmail.com>
2025-12-23 21:39:41 +0530

    added programs (basic for interviews)

---
+++ Programs/src/ReverseAString.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ReverseAString {
+
+    public static String reverseString(String input) {
+
+        char[] chars = input.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+
+        while(left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(reverseString(input));
+        scanner.close();
+    }
+}
